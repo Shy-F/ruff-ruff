87cc7a3b5b61a3e05c42cdf9639117b6f6c144e0
toshetasevski <toshetasevski@Toshes-MacBook-Pro.local>
2021-12-10 23:09:31 -0500

    testing

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,10 @@
+
 // we created a API key and function thta give us back the city we are looking for 
 document.querySelector('.btn').addEventListener('click', function () {
     var city = document.getElementById('city-search').value
     getCity(city)
 })
+
 
 function getCity(city) {
     fetch('http://open.mapquestapi.com/nominatim/v1/search.php?key=sCFiupNXfYSdEqiHE3MpAlyL0c8FzCFs&format=json&q=' + city)
@@ -12,4 +14,13 @@ function getCity(city) {
         .then(function (data) {
             console.log(data);
         })
+
 }
+
+
+
+
+
+
+
+
