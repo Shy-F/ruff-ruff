4e4c688e6f69ee7a507eb58add7ecafa00e25610
Shy <fleetwoodshybreasia@gmail.com>
2021-12-14 15:57:51 -0500

    bone-appetit

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,16 @@
-//slider (index.html)
-$(document).ready(function(){
-    $('.slider').slider();
-});
+// we created a API key and function thta give us back the city we are looking for 
+document.querySelector('.btn').addEventListener('click', function () {
+    var city = document.getElementById('city-search').value
+    getCity(city)
+    var autocomplete = 
+})
+
+function getCity(city) {
+    fetch('http://open.mapquestapi.com/nominatim/v1/search.php?key=sCFiupNXfYSdEqiHE3MpAlyL0c8FzCFs&format=json&q=' + city)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+}
