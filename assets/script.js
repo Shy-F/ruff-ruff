c768de5ad09908fb2a26d4e456a0af499487022f
Shy <fleetwoodshybreasia@gmail.com>
2021-12-18 12:03:08 -0500

    added s to http in script

--- assets/script.js
+++ assets/script.js
@@ -8,7 +8,7 @@ var citySearch = document.getElementById('citySearch')
 
 //we created a API key and function thta give us back the city we are looking for
 function getCity(city) {
-    fetch('http://open.mapquestapi.com/nominatim/v1/search.php?key=sCFiupNXfYSdEqiHE3MpAlyL0c8FzCFs&format=json&q=' + city)
+    fetch('httpS://open.mapquestapi.com/nominatim/v1/search.php?key=sCFiupNXfYSdEqiHE3MpAlyL0c8FzCFs&format=json&q=' + city)
         .then(function (response) {
             return response.json()
         })
